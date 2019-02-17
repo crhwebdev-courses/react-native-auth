6d20a0f1924e0ca585366e57b1947bf7ae792ed5
Carl Hain <crhain@gmail.com>
2019-02-17 11:35:36 -0600

    Downgraded version of firebase to 5.0.3 because the latest versions do not work with es6 modules

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Button, Card, CardSection, Header } from './components/common';
 import firebase from 'firebase';
+import { Button, Card, CardSection, Header } from './components/common';
 import Config from '../.config';
 
 class App extends Component {
