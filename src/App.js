5e62ee127af0aeb6cbe508d5ed01409417e6ee7b
Carl Hain <crhain@gmail.com>
2019-02-17 10:45:04 -0600

    Add firebase config and initialization to App.js; configuration pulls from .config.js file to protect credentials

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Button, Card, CardSection, Header } from './components/common';
+import firebase from 'firebase';
+import Config from '../.config';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: Config.API_KEY,
+      authDomain: Config.AUTH_DOMAIN,
+      databaseURL: Config.DATABASE_URL,
+      projectId: Config.PROJECT_ID,
+      storageBucket: Config.STORAGE_BUCKET,
+      messagingSenderId: Config.MESSEGING_SENDER_ID
+    });
+  }
+
   render() {
     return (
       <View>
