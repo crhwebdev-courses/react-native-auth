9da08efc3c00b844793358fad7b332af40a82189
Carl Hain <crhain@gmail.com>
2019-02-17 19:57:03 -0600

    Add basic authentication flow with simple error message when 'log in' button on LoginForm pressed

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
+import firebase from 'firebase';
 import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-  state = { email: '', password: '' };
+  state = { email: '', password: '', error: '' };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+
+    this.setState({ error: '' });
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        //failed to sign in
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(email, password)
+          .catch(() => {
+            //failed to create new account
+            this.setState({ error: 'Authentication Failed.' });
+          });
+      });
+  }
 
   render() {
     return (
@@ -26,14 +48,22 @@ class LoginForm extends Component {
           />
         </CardSection>
 
-        <CardSection />
+        <Text style={styles.errorTextStyle}>{this.state.error}</Text>
 
         <CardSection>
-          <Button>Log in</Button>
+          <Button onPress={this.onButtonPress.bind(this)}>Log in</Button>
         </CardSection>
       </Card>
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
 
 export default LoginForm;
